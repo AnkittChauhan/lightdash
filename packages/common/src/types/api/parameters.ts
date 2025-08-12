23c128659f04e7431517bf55a163910f6cdfcae5
Yegor Borisenco <pimmey@pimmey.com>
2025-08-12 20:12:31 +0800

    wip: project parameters api

--- packages/common/src/types/api/parameters.ts
+++ packages/common/src/types/api/parameters.ts
@@ -1,6 +1,19 @@
+import type { KnexPaginatedData } from '../knex-paginate';
 import type { LightdashProjectParameter } from '../lightdashProjectConfig';
 
 export type ApiGetProjectParametersResults = Record<
     string,
     LightdashProjectParameter
+>;
+
+export type ProjectParameterSummary = {
+    name: string;
+    label: string;
+    description?: string;
+    default?: string | string[];
+    createdAt: Date;
+};
+
+export type ApiGetProjectParametersListResults = KnexPaginatedData<
+    ProjectParameterSummary[]
 >;
